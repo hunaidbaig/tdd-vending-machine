e34a69a9e6fd5d3deb0ba16850595b3ddd694e66
Hunaid baig <hunaid.baig@xloopdigital.com>
2023-04-12 22:48:35 +0500

    I have passed my first test case

--- src/Machine.js
+++ src/Machine.js
@@ -1,9 +1,14 @@
 module.exports = class Machine {
     constructor() {
-
+        this.items = [
+            {'crisps': 100},
+            {'chocolate': 350},
+            {'mints': 70}
+        ];
     }
 
 
     seeSelections() {
+        return this.items;
     }
 };
