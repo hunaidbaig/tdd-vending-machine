f27ae015261151dce7e769704a813fb212a6aa1a
Hunaid baig <hunaid.baig@xloopdigital.com>
2023-04-12 23:23:08 +0500

    My second test case failed

--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,6 +1,7 @@
 const Machine = require('../src/Machine');
 
 describe('the vending machine', () => {
+    // 1 test case
     it('should have items to purchase', () => {
         // setup
         const machine = new Machine();
@@ -12,5 +13,22 @@ describe('the vending machine', () => {
         // assert
         expect(expected).toEqual(actual);
     });
+
+    // 2nd test case
+    it('show me, how much money I have deposited', () => {
+        // setup
+        const machine = new Machine();
+        const myDeposit = 100;
+        const notAccepted = 1000;
+        // exercise
+        const actualAmount = machine.deposit(myDeposit);
+        const notAcceptedAmount = machine.deposit(notAccepted);
+
+        // assert
+        expect(`You have deposited Rs ${myDeposit}`).toEqual(actualAmount);
+        expect(`We accept bills in these amounts: 10, 20, 50, 100, 500`).toEqual(notAcceptedAmount);
+    });
+
+
 
 });
