d1f0e0a0cc44bf3d445a9aa92e93d5c1164a584c
Hunaid baig <hunaid.baig@xloopdigital.com>
2023-04-12 23:23:47 +0500

    I have passed my second test case

--- src/Machine.js
+++ src/Machine.js
@@ -5,8 +5,19 @@ module.exports = class Machine {
             {'chocolate': 350},
             {'mints': 70}
         ];
+
+        this.amount = 0;
     }
 
+    deposit(myDeposit) {
+        const acceptedBill = [10, 20, 50, 100, 500];
+        if(acceptedBill.includes(myDeposit)){
+            this.amount += myDeposit;
+            return `You have deposited Rs ${this.amount}`;
+        }else{
+            return 'We accept bills in these amounts: 10, 20, 50, 100, 500'
+        }
+    }
 
     seeSelections() {
         return this.items;
